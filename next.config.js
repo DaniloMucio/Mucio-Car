05896fd029093e2974ea7c1a4842de62e9582b96
DaniloMucio <danilomucio@gmail.com>
2025-03-10 12:15:57 -0300

    fix: configuração do Next.js para deploy na Vercel

--- next.config.js
+++ next.config.js
@@ -1,30 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn-icons-png.flaticon.com',
-        pathname: '**',
-      },
-    ],
+    domains: ['images.unsplash.com'],
   },
+  output: 'standalone',
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig 
